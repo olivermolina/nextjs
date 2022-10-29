0e310ca592989d663cf02e89114180beb0dc249b
olivermolina <olivermolina10@gmail.com>
2022-10-29 09:20:08 +0800

    fix: build errors

--- src/components/Profile/index.ts
+++ src/components/Profile/index.ts
@@ -1,4 +1,3 @@
 export { default as ProfileMenu } from './ProfileMenu';
 export { default as ProfileDetails } from './ProfileDetails';
 export { default as Referral } from './Referral';
-export { default as WithdrawFunds } from './WithdrawFunds';
